21b7beb96317ae7dce9fba093959b907f7894fe5
wit4191 <tiger.kmitl@yahoo.com>
2014-03-13 22:00:10 +0700

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -2,18 +2,20 @@ import sys
 from turtle import *
 
 class Disk:
-    def __init__(self, name, x,y,height,width):
+    def __init__(self, name, x,y,height,width,color):
         self.name = name
         self.x = x
         self.y = y
         self.height = height
         self.width = width
+        self.color = color
         up()
         
     def showdisk(self):
         
         goto(self.x,self.y)
-        color("black")
+        begin_fill()
+        color(self.color)
         down()
         forward(self.width/2)
         left(90)
@@ -24,26 +26,20 @@ class Disk:
         forward(self.height)
         left(90)
         forward(self.width/2)
+        end_fill()
         up()
 
 
     def newpos(self,x,y):
         self.x = x
         self.y = y
-        goto(self.x)
-        goto(self.y)
         
-    
     def getH(self):
         return self.height
-        
-    def getW(self):
-        return self.width
-        
-    
 
     def cleardisk(self):
         goto(self.x,self.y)
+        begin_fill()
         color("white")
         down()
         forward(self.width/2)
@@ -55,13 +51,80 @@ class Disk:
         forward(self.height)
         left(90)
         forward(self.width/2)
+        end_fill()
         up()
 
 
-r = Disk('a',0,0,30,90)
+'''r = Disk('a',0,0,30,90,"red")
 s = Disk('b',0,30,30,45)
 r.showdisk()
-r.cleardisk()
+r.cleardisk()'''
+
+class Pole:
+    def __init__(self, name,stack,topPos ,x,y,thickness,length,color):
+        self.name = name
+        self.stack = stack
+        self.topPos = topPos
+        self.x = x
+        self.y = y
+        self.thickness = thickness
+        self.length = length
+        self.color = color
+
+    def showpole(self):
+        goto(self.x,self.y)
+        color("black")
+        down()
+        begin_fill()
+        forward(self.thickness/2)
+        left(90)
+        forward(self.length)
+        left(90)
+        forward(self.thickness)
+        left(90)
+        forward(self.length)
+        left(90)
+        forward(self.thickness/2)
+        end_fill()
+        up()
+
+    def pushdisk(self,disk):
+        
+        self.stack.append(disk)
+        disk.newpos(self.x , self.y+ (self.topPos*disk.height))
+        disk.showdisk()
+        self.topPos += 1
+
+    def popdisk(self):
+        a = self.stack.pop()
+        a.cleardisk()
+        self.topPos -= 1
+
+
+        
+        
+
+        
+
+        
+
+p = Pole("a",[],0,0,0,20,120,"black")
+p.showpole()
+
+a = Disk("b",0,0,30,90,"red")
+p.pushdisk(a)
+p.pushdisk(Disk("b",0,0,30,90,"blue"))
+
+p.popdisk()
+p.popdisk()
+
+
+#s.showdisk()
+
+
+
+
+        
 
 
 
